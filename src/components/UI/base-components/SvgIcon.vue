0865412872c62c2b5b86bc80a4de672ca7fd0292
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-11-12 19:27:53 +0200

    add Common icon template

---
+++ src/components/UI/base-components/SvgIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <component :is="icon"></component>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const icon = defineAsyncComponent(() => import(`@/assets/images/icons/${props.name}.svg`));
+
+
+</script>
+
+<style lang="scss">
+.icon {
+  width: 24px;
+  height: 24px;
+
+  &>svg {
+    object-fit: contain;
+  }
+
+}
+</style>
