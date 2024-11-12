cb9535af72a9ee150fa498d1f02b3e2b2f84ba71
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-11-12 19:35:58 +0200

    add vite-svg-loader to project, add corrects to vite config, add scss to project -  add corrects to configs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import '@/assets/main.css';
 import { createApp } from 'vue';
 
 import router from './router';
 import store from '@/store/index.js';
 
+import './assets/styles/main.scss';
+
+
 import BaseButton from '@/components/UI/base-components/BaseButton.vue';
 import BaseDialog from '@/components/UI/base-components/BaseDialog.vue';
 import BaseSpinner from '@/components/UI/base-components/BaseSpinner.vue';
+import SvgIcon from './components/UI/base-components/SvgIcon.vue';
 
 import App from './App.vue';
 
@@ -19,5 +22,6 @@ app.use(store);
 app.component('BaseButton', BaseButton);
 app.component('BaseDialog', BaseDialog);
 app.component('BaseSpinner', BaseSpinner);
+app.component('SvgIcon', SvgIcon);
 
 app.mount('#app');
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -7,13 +7,17 @@
     </div>
     <p v-if="!columnsToDisplay.length">Choose some filter points to display tasks</p>
     <div v-else class="board">
-      <div v-for="column in columnsToDisplay" :class="['board_column', 'board_column--' + column.toLowerCase()]">
-        <header>
+      <div v-for="column in columnsToDisplay" :class="['board_column', 'board_column--' + column.toLowerCase()]"
+        :style="boardColumnWidth">
+        <header class="board_column-header">
           <h3>{{ column }}</h3>
         </header>
-        <div v-for="task in tasks">
-          <TaskItem v-if="column === task.status" :key="task.id" :id="task.id" :title="task.title"
-            :description="task.description" :status="task.status" />
+        <div class="board_column-content">
+          <div class="task-wrapper" v-for="task in tasks">
+            <TaskItem v-if="column === task.status" :key="task.id" :id="task.id" :title="task.title"
+              :description="task.description" :status="task.status" />
+          </div>
+
         </div>
       </div>
     </div>
@@ -54,6 +58,11 @@ export default {
   },
 
   computed: {
+    boardColumnWidth() {
+      let width = (100 / this.columnsToDisplay.length) + '%';
+      let colWidth = `width: calc( ${width} - 10px)`;
+      return colWidth;
+    },
     toDoList() {
       let toDoItems = this.tasks.filter((task) => task.status === 'toDo');
       return toDoItems;
@@ -67,6 +76,7 @@ export default {
       return doneItems;
     }
   },
+
   methods: {
     displayColumns(columnsList) {
       console.log(columnsList);
@@ -85,7 +95,6 @@ export default {
   created() {
     this.getTasks();
   }
-
 }
 </script>
 
@@ -93,13 +102,35 @@ export default {
 .board,
 .board_column {
   display: flex;
-  border: 1px solid black;
-  padding: 5px;
+  border-radius: 5px;
+}
+
+.board {
+  justify-content: space-between;
+  padding: 0 10px 10px;
+  margin: 20px 0;
+  background-color: #46546f;
+
+}
+
+.board_column-header {
+  padding: 15px;
+  background-color: #46546f;
+  text-align: center;
+}
+
+.task-wrapper {
+  margin-bottom: 10px;
 }
 
 .board_column {
   flex-direction: column;
   width: max(33%);
+  background-color: #5c6786;
+}
+
+.board_column-content {
+  padding: 20px clamp(5px, 15px, 15px);
 }
 
 .board_column--todo {
