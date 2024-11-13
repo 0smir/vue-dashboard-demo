6cd645b6569003e09a2d9fff4b4d7dacbdabda60
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-11-13 10:40:37 +0200

    add styles for Sidebar and some logic (close/open)

--- src/components/UI/TheSidebar.vue
+++ src/components/UI/TheSidebar.vue
@@ -1,21 +1,48 @@
 <template>
-  <aside class="sidebar">
+  <aside :class="['sidebar', { open: isExpanded, close: !isExpanded }]">
+    <div class="sidebar__controls-wrapper">
+      <BaseButton class="btn" :alt="sidebarControlAltText" @click="toggleSidebar">
+        <span class="sidebar-nav__icon">
+          <SvgIcon v-if="!isExpanded" name="chevronLeft" class="icon sidebar__control-icon" />
+          <SvgIcon v-else name="chevronRight" class="icon sidebar__control-icon" />
+        </span>
+      </BaseButton>
+    </div>
     <ul class="sidebar-nav">
       <li class="sidebar-nav__item">
-        <router-link to="/"><span class="icon"></span> <span class="sidebar-text">Boards</span></router-link>
+        <router-link class="sidebar-nav__link" to="/">
+          <span class="sidebar-nav__icon">
+            <SvgIcon name="board" class="icon" />
+          </span>
+          <span class="sidebar__link-text">Boards</span></router-link>
       </li>
       <li class="sidebar-nav__item">
-        <router-link to="/"><span class="icon"></span> <span class="sidebar-text">Statistic</span></router-link>
+        <router-link class="sidebar-nav__link" to="/">
+          <span class="sidebar-nav__icon">
+            <SvgIcon name="statistic" class="icon" />
+          </span>
+          <span class="sidebar__link-text">Statistic</span></router-link>
       </li>
       <li class="sidebar-nav__item">
-        <router-link to="/"><span class="icon"></span><span class="sidebar-text">Settings</span></router-link>
+        <router-link class="sidebar-nav__link" to="/">
+          <span class="sidebar-nav__icon">
+            <SvgIcon name="settings" class="icon" />
+          </span>
+          <span class="sidebar__link-text">Settings</span></router-link>
       </li>
       <li class="sidebar-nav__item">
-        <router-link to="/tasks"><span class="icon"></span> <span class="sidebar-text">All Tasks</span></router-link>
+        <router-link class="sidebar-nav__link" to="/tasks">
+          <span class="sidebar-nav__icon">
+            <SvgIcon name="tasks" class="icon" />
+          </span>
+          <span class="sidebar__link-text">All Tasks</span></router-link>
       </li>
       <li class="sidebar-nav__item">
-        <router-link to="/tasks/new">
-          <span class="icon"></span> <span class="sidebar-text">Add New Task</span>
+        <router-link class="sidebar-nav__link" to="/tasks/new">
+          <span class="sidebar-nav__icon">
+            <SvgIcon name="add" class="icon" />
+          </span>
+          <span class="sidebar__link-text">Add New Task</span>
         </router-link>
       </li>
     </ul>
@@ -24,24 +51,86 @@
 
 <script>
 export default {
+  data() {
+    return {
+      isExpanded: true
+    }
+  },
+
+  computed: {
+    sidebarControlAltText() {
+      return this.isExpanded ? 'expanded' : 'collapsed';
+    }
+  },
+  methods: {
+    toggleSidebar() {
+      this.isExpanded = !this.isExpanded;
+    }
+  }
 
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .sidebar {
   background-color: #273850;
-  width: 200px;
+
   padding: 10px 15px;
+
+  &.close {
+    width: 60px;
+    translate: 'width' 0.35s ease;
+
+    .sidebar__link-text {
+      display: none;
+    }
+  }
+
+  &.open {
+    width: 200px;
+    translate: 'width' 0.35s ease;
+
+    .sidebar__link-text {
+      display: inline;
+    }
+  }
+
+  &__controls-wrapper {
+    margin-bottom: 25px;
+  }
+
+  &__control-icon {
+    // width: 15px;
+    // height: 15px;
+  }
 }
+
+
 
 .sidebar-nav {
   list-style: none;
   padding: 0;
   margin: 0;
-}
 
-.sidebar-nav__item {
-  margin-bottom: 10px;
+  &__item {
+    margin-bottom: 10px;
+  }
+
+  &__link {
+    display: flex;
+    align-items: center;
+    font-size: 16px;
+  }
+
+  &__link:hover {
+    .sidebar-nav__icon {
+      transform: scale(1.25);
+    }
+  }
+
+  &__icon {
+    margin-right: 7px;
+  }
+
 }
 </style>
