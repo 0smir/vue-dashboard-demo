ced7daa1636639049b630ca5df73d01a6863eea7
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-11-29 12:18:36 +0200

    add corrects to styles of form

--- src/components/people/AddPersonForm.vue
+++ src/components/people/AddPersonForm.vue
@@ -1,34 +1,41 @@
 <template>
-  <h2>Add New User</h2>
-  <form @submit.prevent="submitForm" class="form">
-    <div class="form-control">
-      <label for="name" class="form-control__label">Name: </label>
-      <input class="form-control__input" id="name" type="text" v-model="user.name.value">
-    </div>
-    <div class="form-control">
-      <label for="lastName" class="form-control__label">Last Name:</label>
-      <input class="form-control__input" id="lastName" type="text" v-model="user.lastName.value">
-    </div>
-    <div class="form-control">
-      <label for="role" class="form-control__label">Position:</label>
-      <select class="form-control__select" name="role" id="position" v-model="user.position.value">
-        <option value="" disabled selected>Select your Role</option>
-        <option class="form-control__select-option" v-for="(role) in positionsList" :key="role" :value="role">
-          {{ role }}
-        </option>
-      </select>
-    </div>
-    <div class="form-control">
-      <label for="project" class="form-control__label">Project: </label>
-      <select class="form-control__select" name="projectName" id="project" v-model="user.project.value">
-        <option value="" disabled selected>Select Project Name</option>
-        <option class="form-control__select-option" v-for="project in projects" :key="project.id" :value="project.id">
-          {{ project.title }}
-        </option>
-      </select>
-    </div>
-    <div class="form-control__btn-wrapper">
-      <BaseButton class="btn btn__default btn--medium btn--dark">Add User</BaseButton>
+  <h2 class="header header--add-person">Add New User</h2>
+  <form @submit.prevent="submitForm" class="form form-add-person">
+    <div class="form__content form__content--add-person">
+      <div class="form-control form-control--name">
+        <label for="name" class="form-control__label">Name: </label>
+        <input class="form-control__input" id="name" type="text" v-model="user.name.value">
+      </div>
+      <div class="form-control form-control--lastname">
+        <label for="lastName" class="form-control__label">Last Name:</label>
+        <input class="form-control__input" id="lastName" type="text" v-model="user.lastName.value">
+      </div>
+      <div class="form-control form-control--email">
+        <label for="email" class="form-control__label">Email:</label>
+        <input class="form-control__input" id="email" type="email" v-model="user.email.value">
+      </div>
+      <div class="form-control form-control--position">
+        <label for="role" class="form-control__label">Position:</label>
+        <select class="form-control__select" name="role" id="position" v-model="user.position.value">
+          <option value="" disabled selected>Select your Role</option>
+          <option class="form-control__select-option" v-for="(role) in positionsList" :key="role" :value="role">
+            {{ role }}
+          </option>
+        </select>
+      </div>
+      <div class="form-control form-control--project">
+        <label for="project" class="form-control__label">Project: </label>
+        <select class="form-control__select" name="projectName" id="project" v-model="user.project.value">
+          <option value="" disabled selected>Select Project</option>
+          <option class="form-control__select-option" v-for="project in projects" :key="project.id" :value="project.id">
+            {{ project.title }}
+          </option>
+        </select>
+      </div>
+      <div class="form-control__btn-wrapper btn-wrapper">
+        <BaseButton class="btn btn__default btn--medium btn--add">Add User</BaseButton>
+        <BaseButton class="btn btn__outlined btn--medium btn--cancel">Cancel</BaseButton>
+      </div>
     </div>
   </form>
 </template>
@@ -45,6 +52,10 @@ export default {
           isValid: true
         },
         lastName: {
+          value: '',
+          isValid: true
+        },
+        email: {
           value: '',
           isValid: true
         },
@@ -108,4 +119,61 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header--add-person {
+  margin: 20px 0 40px;
+  text-align: center;
+  text-transform: uppercase;
+}
+
+.form__content--add-person {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 760px;
+  margin: 0 auto;
+}
+
+.form-control {
+  width: calc(50% - 7px);
+
+  &--email {
+    width: 100%;
+  }
+}
+
+.btn {
+  &-wrapper {
+    display: flex;
+    flex-direction: column;
+    margin-top: 15px;
+    width: 100%;
+
+    @media (min-width: $md) {
+      flex-direction: row;
+      justify-content: flex-end;
+      margin-top: 45px;
+    }
+  }
+
+  &--add {
+    width: 100%;
+    margin-bottom: 10px;
+
+    @media (min-width: $md) {
+      width: auto;
+      order: 2;
+      margin-bottom: 0;
+    }
+  }
+
+  &--cancel {
+    width: 100%;
+
+    @media (min-width: $md) {
+      margin: 0 10px 0 0;
+      width: auto;
+    }
+  }
+}
+</style>
