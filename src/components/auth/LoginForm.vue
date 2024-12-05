19db32c785384e85ec9b117ba00139e102777a78
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-12-05 23:23:11 +0200

    add Login page with content

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="container">
+    <form class="form from--login" @submit.prevent="submitForm">
+      <div class="form__content">
+        <div class="form-control form-control--email">
+          <label for="email" class="form-control__label">Email</label>
+          <input id="email" type="text" autocomplete="off" class="form-control__input" v-model="user.email.value">
+          <p v-if="!user.email.isValid" class="error-text"> Please, enter a valid email address</p>
+        </div>
+        <div class="form-control form-control--password">
+          <label class="form-control__label" for="password">Password:</label>
+          <div class="form-control__input-wrapper form-control__input-wrapper--password">
+            <input class="form-control__input" :type="showPass ? 'text' : 'password'" id="password"
+              v-model.trim="user.password.value" autocomplete="off" />
+            <span class="password-visibility-wrapper flex" @click="showPass = !showPass">
+              <SvgIcon v-if="!showPass" name="eye" class="icon" />
+              <SvgIcon v-else name="eyeSplash" class="icon" />
+            </span>
+          </div>
+        </div>
+        <div class="form-control__btn-wrapper btn-wrapper">
+          <BaseButton class="btn btn__default btn--medium btn--login">Login</BaseButton>
+          <RouterLink class="link link--forget-pass" to="/people/registration">Registration</RouterLink>
+          <RouterLink class="link link--forget-pass" to="/">Forgot password?</RouterLink>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showPass: false,
+      isFormValid: true,
+      user: {
+        email: {
+          value: '',
+          isValid: true
+        },
+        password: {
+          value: '',
+          isValid: true
+        }
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log('submitForm');
+
+      this.isFormValid = true;
+      if (
+        this.user.email.value === '' ||
+        !this.user.email.value.includes('@') ||
+        this.user.email.value.length < 4
+      ) {
+        this.user.email.isValid = false;
+        this.isFormValid = false;
+        return;
+      }
+
+      if (this.user.password.value === '' ||
+        this.user.password.value.length < 5) {
+        this.user.password.isValid = false;
+        this.isFormValid = false;
+        return;
+      }
+
+      const actionPayload = {
+        email: this.user.email,
+        password: this.user.password
+      };
+    }
+  },
+  clearFormFields() {
+    this.user.email = '';
+    this.user.password = '';
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-wrapper {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.btn--login {
+  width: 100%;
+  margin: 20px 0 40px;
+}
+
+.link--forget-pass {
+  color: var(--color-primary);
+}
+</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="registration-logo__wrapper">
+      <SvgIcon name="logo" class="icon icon--logo registration-logo__icon" />
+    </div>
+    <h2 class="title">Login</h2>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/auth/LoginForm.vue';
+export default {
+  components: {
+    LoginForm
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 40px;
+}
+
+.registration-logo {
+  &__wrapper {
+    margin-bottom: 25px;
+  }
+
+  &__icon {
+    color: var(--color-primary);
+    width: 75px;
+    height: 75px;
+  }
+}
+
+
+.title {
+  margin-bottom: 0;
+  text-align: center;
+}
+</style>
