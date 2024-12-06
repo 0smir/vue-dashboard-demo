7eb1a14c3ba585b127a3acc2ad7b627c596b8211
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-12-06 23:28:22 +0200

    add adaptivity for people list template

--- src/components/people/PersonsList.vue
+++ src/components/people/PersonsList.vue
@@ -38,8 +38,20 @@ export default {
 .userslist {
   &__wrapper {
     display: grid;
-    grid-template-columns: repeat(4, calc(25% - 15px));
+    grid-template-columns: 1fr;
     gap: 20px 15px;
+
+    @media (min-width: $sm) {
+      grid-template-columns: repeat(2, calc(50% - 15px));
+    }
+
+    @media (min-width: $md) {
+      grid-template-columns: repeat(3, calc(33% - 15px));
+    }
+
+    @media (min-width: $tablet) {
+      grid-template-columns: repeat(4, calc(25% - 15px));
+    }
   }
 }
 </style>
