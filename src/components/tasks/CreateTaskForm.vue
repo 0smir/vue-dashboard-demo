6bdfbda7ce208e91248004f6de074ebfcf2266cf
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-12-07 21:46:01 +0200

    add error-messages to addTaskForm

--- src/components/tasks/CreateTaskForm.vue
+++ src/components/tasks/CreateTaskForm.vue
@@ -1,41 +1,48 @@
 <template>
   <form @submit.prevent="submitForm" :class="['form', 'form-add-task', 'form-' + className, className]">
     <div class="form__content">
-      <div class="form-control form-control--title">
+      <div :class="['form-control', 'form-control--title', { error: !task.title.isValid }]">
         <label class="form-control__label" for="title">Title</label>
-        <input class="form-control__input" id="title" type="text" v-model="title.value">
+        <input class="form-control__input" id="title" type="text" v-model="task.title.value">
+        <p class="error-text" v-if="!task.title.isValid">{{ task.title.errorMessage }}</p>
       </div>
-      <div class="form-control form-control--project">
+      <div :class="['form-control', 'form-control--project', { error: !task.project.isValid }]">
         <label class="form-control__label" for="project">Select Project: </label>
-        <select class="form-control__select" name="project-name" id="project" v-model="project.value">
+        <select class="form-control__select" name="project-name" id="project" v-model="task.project.value">
           <option v-for="project in projectsList" :value="project.id">{{ project.title }}</option>
         </select>
+        <p class="error-text" v-if="!task.project.isValid">{{ task.project.errorMessage }}</p>
       </div>
-      <div class="form-control form-control--assignee">
+      <div :class="['form-control', 'form-control--assignee', { error: !task.assignee.isValid }]">
         <label class="form-control__label" for="assignee">Assignee</label>
-        <select class="form-control__select" name="assignee" id="assignee" v-model="assignee.value">
+        <select class="form-control__select" name="assignee" id="assignee" v-model="task.assignee.value">
           <option class="form-control__select-option" v-for="person in assigneeList" :value="person.id">
             {{ person.name + ' ' + person.lastName }}
           </option>
         </select>
+        <p class="error-text" v-if="!task.assignee.isValid">{{ task.assignee.errorMessage }}</p>
       </div>
-      <div class="form-control form-control--status">
+      <div :class="['form-control', 'form-control--status', { error: !task.status.isValid }]">
         <label class="form-control__label" for="status">Status</label>
-        <select class="form-control__select" name="task-status" id="status" v-model="status.value">
-          <option class="form-control__select-option" v-for="status in statusList" :value="status">{{ status }}</option>
+        <select class="form-control__select" name="task-status" id="status" v-model="task.status.value">
+          <option class="form-control__select-option" v-for="(status, index) in statusList" :value="status"
+            :key="status">{{ status }}</option>
         </select>
+        <p class="error-text" v-if="!task.status.isValid">{{ task.status.errorMessage }}</p>
       </div>
-      <div class="form-control form-control--priority">
+      <div :class="['form-control', 'form-control--priority', { error: !task.priority.isValid }]">
         <label class="form-control__label" for="priority">Priority</label>
-        <select class="form-control__select" name="task-priority" id="priority" v-model="priority.value">
+        <select class="form-control__select" name="task-priority" id="priority" v-model="task.priority.value">
           <option class="form-control__select-option" v-for="priority in priorityList" :value="priority">{{ priority }}
           </option>
         </select>
+        <p class="error-text" v-if="!task.priority.isValid">{{ task.priority.errorMessage }}</p>
       </div>
-      <div class="form-control form-control--description">
+      <div :class="['form-control', 'form-control--description', { error: !task.description.isValid }]">
         <label class="form-control__label" for="description">Description</label>
         <textarea class="form-control__textarea" name="task-description" id="description" rows="5"
-          v-model="description.value"></textarea>
+          v-model="task.description.value"></textarea>
+        <p class="error-text" v-if="!task.description.isValid">{{ task.description.errorMessage }}</p>
       </div>
       <div class="form-control form-control--btn-wrapper btn-wrapper">
         <BaseButton class="btn btn__default btn--medium btn--add" type="submit">Add Task</BaseButton>
@@ -50,29 +57,35 @@ export default {
   props: ['className'],
   data() {
     return {
-      title: {
-        value: '',
-        isValid: true
-      },
-      project: {
-        value: '',
-        isValid: true
-      },
-      assignee: {
-        value: '',
-        isValid: true
-      },
-      description: {
-        value: '',
-        isValid: true
-      },
-      priority: {
-        value: '',
-        isValid: true
-      },
-      status: {
-        value: '',
-        isValid: true
+      task: {
+        title: {
+          value: '',
+          errorMessage: 'Task title is required and cannot be empty.',
+          isValid: true
+        },
+        project: {
+          value: '',
+          errorMessage: 'Select a project before creating the task.',
+          isValid: true
+        },
+        assignee: {
+          value: '',
+          errorMessage: 'Please assign the task to a valid team member.',
+          isValid: true
+        },
+        priority: {
+          value: 'trivial',
+          isValid: true
+        },
+        status: {
+          value: 'toDo',
+          isValid: true
+        },
+        description: {
+          value: '',
+          errorMessage: 'Task description must be provided for clarity.',
+          isValid: true
+        }
       },
       isFormValid: true
     }
@@ -95,29 +108,35 @@ export default {
     validateForm() {
       this.isFormValid = true;
 
-      if (this.title.value === '') {
-        this.title.isValid = false;
+      if (this.task.title.value === '') {
+        this.task.title.isValid = false;
         this.isFormValid = false;
+        return;
       }
-      if (this.project.value === '') {
-        this.project.isValid = false;
+      if (this.task.project.value === '') {
+        this.task.project.isValid = false;
         this.isFormValid = false;
+        return;
       }
-      if (this.assignee.value === '') {
-        this.assignee.isValid = false;
+      if (this.task.assignee.value === '') {
+        this.task.assignee.isValid = false;
         this.isFormValid = false;
+        return;
       }
-      if (this.description.value === '') {
-        this.description.isValid = false;
+      if (this.task.description.value === '') {
+        this.task.description.isValid = false;
         this.isFormValid = false;
+        return;
       }
-      if (this.priority.value === '') {
-        this.priority.isValid = false;
+      if (this.task.priority.value === '') {
+        this.task.priority.isValid = false;
         this.isFormValid = false;
+        return;
       }
-      if (this.status.value === '') {
-        this.status.isValid = false;
+      if (this.task.status.value === '') {
+        this.task.status.isValid = false;
         this.isFormValid = false;
+        return;
       }
     },
 
@@ -128,16 +147,16 @@ export default {
     },
 
     addTask() {
-      let taskID = this.project.value.slice(0, 1).toUpperCase() + '-' + new Date().getTime();
+      let taskID = this.task.project.value.slice(0, 1).toUpperCase() + '-' + new Date().getTime();
       console.log(taskID);
       let formData = {
         id: taskID,
-        title: this.title.value,
-        project: this.project.value,
-        assignee: this.assignee.value,
-        description: this.description.value,
-        priority: this.priority.value,
-        status: this.status.value
+        title: this.task.title.value,
+        project: this.task.project.value,
+        assignee: this.task.assignee.value,
+        description: this.task.description.value,
+        priority: this.task.priority.value,
+        status: this.task.status.value
       };
 
       this.$store.dispatch('tasks/addTask', formData);
@@ -145,15 +164,14 @@ export default {
     },
 
     clearFormFields() {
-      this.title.value = '';
-      this.project.value = '';
-      this.assignee.value = '';
-      this.description.value = '';
-      this.priority.value = '';
-      this.status.value = '';
+      this.task.title.value = '';
+      this.task.project.value = '';
+      this.task.assignee.value = '';
+      this.task.description.value = '';
+      this.task.priority.value = '';
+      this.task.status.value = '';
     }
   }
-
 }
 </script>
 
