6a2abe435e6af239723b306b625ec61592df6711
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-12-09 22:22:25 +0200

    add main structure and styles of all tasks page

--- src/pages/AllTasksPage.vue
+++ src/pages/AllTasksPage.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>
-    All tasks Page
+  <div class="container">
+    <h2 class="title">All Tasks list</h2>
+    <ul class="tasks-list__wrapper">
+      <li class="tasks-list__item" v-for="task in tasks">
+        <TaskItem :task="task" :key="task.id" />
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+import TaskItem from '@/components/tasks/TaskItem.vue';
 export default {
-
+  components: {
+    TaskItem
+  },
+  data() {
+    return {
+      tasks: this.$store.getters['tasks/getTasksList']
+    }
+  },
+  methods: {
+    async loadTasks(params) {
+      try {
+        await this.$store.dispatch('tasks/loadTasks');
+      } catch (error) {
+        this.error = error.message || 'Smth went wrong!';
+      }
+    }
+  },
+  mounted() {
+    this.loadTasks();
+  }
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.tasks-list {
+  &__wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    row-gap: 20px;
+    padding: 0;
+    list-style: none;
+  }
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    width: 100%;
+
+    @media (min-width: $sm) {
+      width: calc(50% - 10px);
+    }
+  }
+}
+</style>
