14cd71f2f48915b58c48e4beae6f7bd0c0b64614
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-12-10 13:54:14 +0200

    remove commented code

--- src/store/people/mutations.js
+++ src/store/people/mutations.js
@@ -3,8 +3,6 @@ export default {
     state.people.push(payload);
   },
   setEmployees(state, payload) {
-    console.log('payload', payload);
     state.people = payload;
-    console.log('state.people', state.people);
   }
 }
