177cee4ad78bdadd23a686deda239389b91b7d07
Olga Smirnova <o.v.smirnova1987@gmail.com>
2024-12-17 13:03:52 +0200

    add base mark up of base dropdown component

---
+++ src/components/UI/base-components/BaseDropdown.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dropdown dropdown__wrapper">
+    <BaseButton class="">BTN Title</BaseButton>
+    <div class="dropdown__content-wrapper">
+      <ul class="dropdown__content-list">
+        <li class="dropdown__content-item">
+          <slot></slot>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
