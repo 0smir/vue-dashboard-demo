f1487c2ebc63738b9a10fb38dacf12f9584c00f8
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-01-08 15:34:30 +0200

    change order of task actions in Array

--- src/store/tasks/index.js
+++ src/store/tasks/index.js
@@ -10,7 +10,7 @@ export default {
       task: null,
       taskPriorityList: ['blocker', 'critical', 'high', 'medium', 'trivial', 'low'],
       taskStatusList: ['toDo', 'inProgress', 'inReview', 'Testing', 'Done', 'Blocked'],
-      taskActions: ['delate', 'logtime', 'print'],
+      taskActions: ['logtime', 'print', 'delate'],
     }
   },
   mutations: mutations,
