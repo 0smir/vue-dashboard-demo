1b62f6ed21d1d89fb9ce0d20c02a63f420c27f1a
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-01-09 15:05:43 +0200

    add task content component

---
+++ src/components/tasks/TaskContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="task-info__main">
+    <h1 class="task-info__title">{{ taskInfo?.id }}: {{ taskInfo?.title }}</h1>
+    <div class="task-info__descrioption-wrapper">
+      <span class="task-info__label task-info__label--description">Description:</span>
+      <div class="task-info__descrioption">
+        <p class="text">{{ taskInfo?.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['taskInfo']
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
