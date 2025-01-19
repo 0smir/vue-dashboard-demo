96d5a0b4a76f0867e112147561440a0ec9536d1f
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-01-19 12:18:20 +0200

    add styles and conditions for case logget time exceed estimated time

--- src/components/tasks/TaskTreckedTimeDetails.vue
+++ src/components/tasks/TaskTreckedTimeDetails.vue
@@ -2,12 +2,13 @@
   <div class="task-details task-details__time task-details--time-trecked">
     <span v-if="isLabelVisible" class="task-details__label task-details__label--time-trecked">Time tracked: </span>
     <div class="task-details__value task-details__value--time-trecked"> 
-      <div class="task-details__value-indicator-wrapper">
+      <div :class="['task-details__value-indicator-wrapper', {overdue: isTaskOverdue}]">
         <span class="task-details__value-indicator" :style="{'width': loggedTimeIndicatorWidth + '%'}"></span>
       </div>
       <div class="trecked-time-details">
           <span>{{ trecked }} logged</span>
-          <span>{{ remaining }} remaining</span>
+          <span v-if="!isTaskOverdue">{{ remaining }} remaining</span>
+          <span v-else>{{ estimated }} original estimated</span>
         </div>
     </div>
   </div>
@@ -16,20 +17,35 @@
 <script>
 export default {
   props: ['isLabelVisible', 'estimated', 'trecked'],
+  data() {
+    return {
+      isTaskOverdue: !(this.estimated >= this.trecked)
+    }
+  },
   computed: {
     loggedTimeIndicatorWidth() {
+      let width;
       if (!this.estimated || !this.trecked) return 0;
-      let width = (this.trecked / this.estimated) * 100;
+      if (this.isTaskOverdue) {
+        width = (this.estimated / this.trecked) * 100;
+      }else{
+        width = (this.trecked / this.estimated) * 100;
+      }
       return width;
     },
     remaining() {
       let remainingTime = this.estimated - this.trecked;
       return remainingTime;
     }
-  } 
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.task-details__value-indicator-wrapper {
+  &.overdue {
+    background-color: var(--color-inreview);
+  }
+}
 
 </style>
