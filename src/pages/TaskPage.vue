148f8bc93890cb632c3fabffbd71c51915ea2504
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-01-27 20:43:29 +0200

    add conditions to display create and update time of task

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -25,11 +25,11 @@
                             @choose-action="updateTaskParams"
         />
         <TaskReporterDetails :reporter="taskInfo.reporter" />
-        <div class="task-details task-details__time task-details--time-estimated">
+        <div v-if="taskInfo?.estimateTime" class="task-details task-details__time task-details--time-estimated">
           <span class="task-details__label task-details__label--time-estimated">Estimated: </span>
           <span class="task-details__value task-details__value--time-estimated"> {{ taskInfo?.estimateTime + 'h' }}</span>
         </div>
-        <TaskTreckedTimeDetails :isLabelVisible="true" :estimated="taskInfo?.estimateTime" :trecked="taskInfo?.loggedTime"/>
+        <TaskTreckedTimeDetails v-if="taskInfo?.estimateTime && taskInfo?.loggedTime" :isLabelVisible="true" :estimated="taskInfo?.estimateTime" :trecked="taskInfo?.loggedTime"/>
 
         <div v-if="taskInfo?.createdTime" class="task-details task-details__time task-details__time--created">
           <span class="task-details__label task-details__label--time-created">Created: </span>
