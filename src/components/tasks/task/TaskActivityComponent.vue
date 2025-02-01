16e62bdf563eab0838c4d7d13535019360e89e40
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-02-01 20:58:59 +0200

    add activity component items (all, history, comments, wrklog)

---
+++ src/components/tasks/task/TaskActivityComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="section task-activity__wrapper">
+    <h4 class="section__title title">Activity</h4>
+    <div class="tabs">
+      <span class="tabs__label">Show:</span>
+      <BaseButton :class="['tabs__btn', { active: activeItem === 'TaskAllActivitiesComponent' }]" @click="updateFilter('TaskAllActivitiesComponent')">All
+      </BaseButton>
+      <BaseButton :class="['tabs__btn', { active: activeItem === 'TaskCommentsComponent' }]" @click="updateFilter('TaskCommentsComponent')">Comments
+      </BaseButton>
+      <BaseButton :class="['tabs__btn', { active: activeItem === 'TaskHistoryComponent' }]" @click="updateFilter('TaskHistoryComponent')">History
+      </BaseButton>
+      <BaseButton :class="['tabs__btn', { active: activeItem === 'TaskWorkLogComponent' }]" @click="updateFilter('TaskWorkLogComponent')">Work log
+      </BaseButton>
+    </div>
+    <div class="tabs__content">
+      <component :is="activeItem"></component>
+    </div>
+    
+  </section>
+</template>
+
+<script>
+import TaskAllActivitiesComponent from '@/components/tasks/task/TaskAllActivitiesComponent.vue';
+import TaskCommentsComponent from '@/components/tasks/task/TaskCommentsComponent.vue';
+import TaskHistoryComponent from '@/components/tasks/task/TaskHistoryComponent.vue';
+import TaskWorkLogComponent from '@/components/tasks/task/TaskWorkLogComponent.vue';
+
+export default {
+  components: {
+    TaskAllActivitiesComponent,
+    TaskCommentsComponent,
+    TaskHistoryComponent,
+    TaskWorkLogComponent
+  },
+  props: ['activity'],
+  data() {
+    return {
+      activeItem: 'TaskAllActivitiesComponent'
+    }
+  },
+  methods: {
+    updateFilter(componentName) {
+      this.activeItem = componentName;
+    }
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.section {
+  &__title {
+    font-size: 18px;
+    margin-bottom: 20px;
+  } 
+}
+.tabs {
+  &__content {
+    padding-top: 20px;
+  }
+ 
+  &__label {
+    margin-right: 15px;
+  }
+}
+</style>
