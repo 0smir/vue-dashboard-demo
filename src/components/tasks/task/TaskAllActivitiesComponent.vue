a84a73576b1b342e3e7d7b7270528898cd13c71d
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-02-06 23:06:47 +0200

    add commentItem and workLogItem components, optimize code to avoid redundant tag-wraps

--- src/components/tasks/task/TaskAllActivitiesComponent.vue
+++ src/components/tasks/task/TaskAllActivitiesComponent.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    All activities
+  <div class="content">
+    <ul>
+      <li v-for="item in activity">
+        <template v-if="item.mode === 'comment'">
+          <CommentItem :comment="item"/>
+        </template>
+        <template v-else-if="item.mode === 'logTime'">
+          <WorkLogItem :workLog="item"/>
+        </template>
+        <template v-else>
+          {{ item.mode }}
+        </template>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import CommentItem from '@/components/tasks/task/CommentItem.vue';
+import WorkLogItem from '@/components/tasks/task/WorkLogItem.vue';
+export default {
+  components: {
+    CommentItem,
+    WorkLogItem
+  },
+  props: ['taskID', 'activity']
+}
 </script>
 
 <style lang="scss" scoped>
