ccb229832c782b6512fdb8f5e86f88db98584f02
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-02-07 15:30:59 +0200

    include HistoryItem component to template of history tab content, add styles and logic

--- src/components/tasks/task/TaskHistoryComponent.vue
+++ src/components/tasks/task/TaskHistoryComponent.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    History Content
+   <div class="history__list-wrapper">
+    <ul class="history__list">
+      <li class="history__list-item" v-for="item in activity">
+        <HistoryItem :item="item"/>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import HistoryItem from '@/components/tasks/task/HistoryItem.vue';
   export default {
-    
-  }
+    components: {
+      HistoryItem
+  },
+  props: ['activity']
+}
 </script>
 
 <style lang="scss" scoped>
