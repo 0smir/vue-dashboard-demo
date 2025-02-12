6da4e79b469ead32ea51add315bd55d825e42861
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-02-12 16:06:22 +0200

    add additional params (size and theme), add styles for theme

--- src/components/auth/UserProfileImg.vue
+++ src/components/auth/UserProfileImg.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="user-profile__image-wrapper rounded">
+    <div :class="['user-profile__image-wrapper', 'rounded', {[theme]: theme}, {[size]: size}]" 
+        :title="userFullName">
       <span>{{ userInitials }}</span>
     </div>
   </div>
@@ -9,31 +10,48 @@
 <script>
   
 export default {
-  props: ['userInfo'],
+  props: ['userInfo', 'theme', 'size'],
   computed: {
     userInitials() {
       let initials = this?.userInfo?.name.charAt(0).toUpperCase() + this?.userInfo?.lastName.charAt(0).toUpperCase();
       return initials;
+    },
+    userFullName() {
+      return `${this?.userInfo?.name.toUpperCase()} ${this?.userInfo?.lastName.toUpperCase()}`;
+    }
+  },
+  mounted() {
+    if (this.theme) {
+      console.log(this.theme);
+    } else {
+      console.log('no theme');
     }
   }
 }
 </script>
 
 <style lang="scss">
-.user-profile {
-  &__image-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 5px;
-    width: 34px;
-    height: 34px;
-    border: 2px solid var(--color-dark-bg);
-    font-size: 16px;
-    color: var(--color-text-dark);
-    background-color: var(--color-white);
-    font-weight: 400;
-    padding: 2px;
+.user-profile__image-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5px;
+  width: 34px;
+  height: 34px;
+  border: 2px solid;
+  font-size: 16px;
+  color: var(--color-text-dark);
+  background-color: var(--color-white);
+  font-weight: 400;
+  padding: 2px;
+
+  &.dark {
+    border-color: var(--color-dark-bg);
   }
+  &.light {
+    border-color: var(--color-primary);
+    background-color: var(--color-primary-light);
+  }
+
 }
 </style>
