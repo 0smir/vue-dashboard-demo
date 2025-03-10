498c3d0e7a0173c379dd18ac5e5fc87318de948e
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-03-10 16:41:53 +0200

    create BoardsPage template (to display list of all existing boards list)

---
+++ src/pages/BoardsPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Boards list</h1>
+    <router-link to="">
+
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
