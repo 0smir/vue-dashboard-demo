dd7333b91b734e5b5969f271317da91895938ae9
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-03-10 16:38:42 +0200

    create NewBoardPage, include create form component

---
+++ src/pages/NewBoardPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h2 class="title page-title">Create new board</h2>
+   <NewBoardForm />
+  </div>
+</template>
+
+<script>
+import NewBoardForm from '@/components/board/NewBoardForm.vue';
+export default {
+  components: {
+    NewBoardForm
+  }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
