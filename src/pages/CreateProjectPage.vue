abd27d5906c3da98368fd0230f53d4fce716332b
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-11 21:41:23 +0300

    add function to load all employees list, employees list set as a prop of createProjectForm, add conditions of paje content  render (spinner/content)

--- src/pages/CreateProjectPage.vue
+++ src/pages/CreateProjectPage.vue
@@ -1,20 +1,47 @@
 <template>
-  <div class="container">
+  <div v-if="employeesList.length" class="container">
     <h2 class="title page-title">Create new project</h2>
-    <CreateProjectForm />
+    <CreateProjectForm :employees="employeesList"
+    />
+  </div>
+  <div v-else class="container">
+    <BaseSpinner/>
   </div>
 </template>
 
 <script>
+import BaseSpinner from '@/components/UI/base-components/BaseSpinner.vue';
 import CreateProjectForm from '@/components/project/CreateProjectForm.vue';
 export default {
   components: {
+    BaseSpinner,
     CreateProjectForm
   },
   data() {
     return {
-
+      isLoading: false
     }
+  },
+  computed: {
+    employeesList() {
+      console.log(this.$store.getters['people/getEmployeesList'].length);
+      
+      return this.$store.getters['people/getEmployeesList'];
+    }
+  },
+  methods: {
+    async loadEmployeesList() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('people/loadEmployeesList');
+        this.isLoading = false;
+      } catch (error) {
+        this.error = error.message || 'Smth went wrong, faled to  EmployeesList fetch!';
+      }
+    }
+  },
+  created() {
+    this.loadEmployeesList();
   }
 
 }
