efdf527bdf4be0ee51125c33bee61130713d875d
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-12 16:17:24 +0300

    complite position list with HR position

--- src/store/people/index.js
+++ src/store/people/index.js
@@ -7,7 +7,7 @@ export default {
   state() {
     return {
       people: JSON.parse(localStorage.getItem('employees')) ? JSON.parse(localStorage.getItem('employees')) : [],
-      positionsList: ['Front-End', 'Back-End', 'QA', 'Manager', 'Product Owner', 'SEO', 'Designer']
+      positionsList: ['Front-End', 'Back-End', 'QA', 'Manager', 'Product Owner', 'SEO', 'Designer', 'HR']
     }
   },
   actions,
