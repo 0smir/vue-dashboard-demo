f00dedd4f7cb73a7a6d1735e5c3cadf057bb7b2a
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-14 18:27:35 +0300

    add base template of Project with mimimal content

---
+++ src/pages/ProjectPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Project page {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props:["id"]
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
