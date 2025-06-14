239cf7c25dc8ef4b6b47a5aa7727fe412857f701
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-06-14 18:29:38 +0300

    add sucess message which shoes in case of project successfully created

--- src/pages/CreateProjectPage.vue
+++ src/pages/CreateProjectPage.vue
@@ -1,25 +1,39 @@
 <template>
-  <div v-if="employeesList.length" class="container">
-    <h2 class="title page-title">Create new project</h2>
-    <CreateProjectForm :employees="employeesList"
-    />
+  <div class="container" v-if="!isProjCreationSuccess">
+    <div v-if="employeesList.length">
+      <h2 class="title page-title">Create new project</h2>
+      <CreateProjectForm :employees="employeesList" @on-project-create="projectCreated"
+      />
+    </div>
+    <div v-else>
+      <BaseSpinner/>
+    </div>
   </div>
   <div v-else class="container">
-    <BaseSpinner/>
+    <BaseSuccessMessage :options="successOptions"/>
   </div>
 </template>
 
 <script>
 import BaseSpinner from '@/components/UI/base-components/BaseSpinner.vue';
 import CreateProjectForm from '@/components/project/CreateProjectForm.vue';
+import BaseSuccessMessage from '@/components/UI/base-components/BaseSuccessMessage.vue';
+
 export default {
   components: {
     BaseSpinner,
-    CreateProjectForm
+    CreateProjectForm,
+    BaseSuccessMessage
   },
   data() {
     return {
-      isLoading: false
+      isLoading: false,
+      isProjCreationSuccess: false,
+      successOptions: {
+        type: 'project',
+        title: '',
+        id: ''
+      }
     }
   },
   computed: {
@@ -36,6 +50,13 @@ export default {
       } catch (error) {
         this.error = error.message || 'Smth went wrong, faled to  EmployeesList fetch!';
       }
+    },
+    projectCreated(options) {
+      console.log(options);
+      let { created, projParams } = options;
+      
+      this.isProjCreationSuccess = created;
+      this.successOptions = Object.assign(this.successOptions, projParams);
     }
   },
   created() {
