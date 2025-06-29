2466c96ab51c54575cd7e0cd3ada31dfab0b01aa
o.smirnova <o.v.smirnova1987@gmail.com>
2025-06-29 23:27:23 +0300

    add main structure and functionality for all boards page

--- src/pages/BoardsPage.vue
+++ src/pages/BoardsPage.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="container">
     <h1>Boards list</h1>
-    <router-link to="">
+    <ul>
+      <li class="board-link__wrapper" v-for="board in boardsList">
+        <router-link  :to="getUrl(board.id)" 
+                      :key="board.id"
+                      class="board-link"
+        >
+          {{ board.title }}
+        </router-link>
+      </li>
+    </ul>
 
-    </router-link>
   </div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  computed: {
+    boardsList(){
+      return this.$store.getters["boards/getBoardsList"];
+    }
+  },
+  methods: {
+    async LoadAllBoardsData(){
+      this.$store.dispatch("boards/LoadAllBoardsData");
+    },
+    getUrl(id){
+      return `/boards/${id}`;
+    }
+  },
+
+  created(){
+    this.LoadAllBoardsData();
   }
+}
 </script>
 
 <style lang="scss" scoped>
+.board-link {
+  color: var(--color-primary);
+  // &__wrapper {
+    
+  // }
+}
 
 </style>
