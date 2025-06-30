0669ce1041e68b207803bd246800395d7838a409
o.smirnova <o.v.smirnova1987@gmail.com>
2025-06-30 13:28:23 +0300

    add corrects to srtyles of tabs

--- src/components/tasks/TasksFilter.vue
+++ src/components/tasks/TasksFilter.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="tabs">
-    <BaseButton :class="['tabs__btn', { active: activeItem === 'all' }]" @click="updateFilter('all')">All
-    </BaseButton>
-    <BaseButton v-for="item in filterItemsList" :key="item" @click="updateFilter(item)"
-      :class="['tabs__btn', { active: activeItem === item }]">{{ item }}</BaseButton>
+    <ul class="tabs__list">
+      <li class="tabs__item"> 
+        <BaseButton :class="['tabs__btn', { active: activeItem === 'all' }]" 
+                    @click="updateFilter('all')">
+                    All
+        </BaseButton>
+      </li>
+      <li class="tabs__item" v-for="item in filterItemsList" :key="item">
+         <BaseButton  @click="updateFilter(item)"
+          :class="['tabs__btn', { active: activeItem === item }]">
+          {{ item }}
+        </BaseButton>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,5 +32,21 @@ export default {
 <style lang="scss" scoped>
 .tabs {
   margin-bottom: 20px;
+  overflow-x: hidden;
+
+  &__list{
+    display: flex;
+    width: 100%;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  &__item {
+    margin-right: 5px;
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
 }
 </style>
