87d0edc24aa3d0d6a88e7a290e6f87cdf90d7821
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-07-07 20:27:37 +0300

    Load user info, display fullName of person

--- src/pages/PersonPage.vue
+++ src/pages/PersonPage.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    Person
+    Person: {{fullName}}
   </div>
 </template>
 
 <script>
 export default {
+  props: ['id'],
+  data() {
+    return {
+      error: null
+    }
+  },
+  computed: {
+    userInfo() {
+      return this.$store.getters['users/getUserInfo'];
+    },
+    fullName() {
+      return `${this.userInfo.name} ${this.userInfo.lastName}`;
+    }
+  },
+  methods: {
+    async loadUserInfo() {
+      try {
+        await this.$store.dispatch('users/loadUserData', this.id);
+      } catch (error) {
+        this.error = error.message || 'Failed to fetch'
+      }
+    },
+  },
+  created() {
+    this.loadUserInfo();
+  }
 
 }
 </script>
