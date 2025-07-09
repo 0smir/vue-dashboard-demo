121146616e62f0e3f870683379ba27696f890967
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-07-09 20:01:00 +0300

    add project info component (contain project details)

---
+++ src/components/project/ProjectInfoComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>Title: {{ project.title }}</div>
+    <div>ID: {{ project.id }}</div>
+    <div>Description: {{ project.description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
