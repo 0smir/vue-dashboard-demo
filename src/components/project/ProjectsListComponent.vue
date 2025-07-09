9c0be72acdfe1d85371f045a6cc55e98b1ef277f
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-07-09 20:00:05 +0300

    add project list item

---
+++ src/components/project/ProjectsListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="projects__list-item">
+    <BaseButton @click="chooseProject"> {{  project.title }}</BaseButton>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  emits: ['change-active-project'],
+  methods: {
+    chooseProject() {
+      this.$emit('change-active-project', this.project);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
