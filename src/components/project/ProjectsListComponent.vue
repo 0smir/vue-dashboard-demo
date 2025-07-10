446f3bbc000b5029edfca274ca900c830ca16e89
Olga Smirnova <o.v.smirnova1987@gmail.com>
2025-07-10 13:29:25 +0300

    add base styles for buttons, add props to diaplay active item

--- src/components/project/ProjectsListComponent.vue
+++ src/components/project/ProjectsListComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <li class="projects__list-item">
-    <BaseButton @click="chooseProject"> {{  project.title }}</BaseButton>
+    <BaseButton :class="['projects__project-button', {'active': isActive}]" @click="chooseProject"> {{  project.title }}</BaseButton>
   </li>
 </template>
 
 <script>
 export default {
-  props: ['project'],
+  props: ['project', 'isActive'],
   emits: ['change-active-project'],
   methods: {
     chooseProject() {
@@ -17,5 +17,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.projects {
+  &__list-item{
+   margin-bottom: 10px;
+  }
+  &__project-button {
+    position: relative;
+    width: 100%;
+    padding: 15px;
+    border: 1px solid var(--color-primary);
+    border-right: none;
+    border-radius: var(--border-radius-small);
+    background-color: var(--color-white);
+    color: var(--color-primary);
+    font-weight: 700;
+    text-transform: uppercase;
+    &:hover{
+      background-color: var(--color-primary-light);
+    }
+    &.active {
+      border: 3px solid var(--color-primary);
+      border-right: 3px solid white;
+      width: calc(100% + 3px);
+    }
+  }
+}
 </style>
